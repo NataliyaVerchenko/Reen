94e3eae0369b6337cf48335c142117a59e9a5a40
NataliaVerchenko <ver4.nata@gmail.com>
2018-01-08 17:29:48 +0000

    create footer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 $( document ).ready(function() {
+    $('.carusel').slick({
+        autoplay:false,
+        autoplaySpeed: 4500,
+        arrows:false,
+        dots:false,
+        speed:1000
+
+    });
+    $('.list-works').slick({
+        autoplay:false,
+        arrows:true,
+        dots:true,
+        speed:1000,
+        slidesToShow: 4,
+        slidesToScroll: 4
+
+
+    });
+
 });
