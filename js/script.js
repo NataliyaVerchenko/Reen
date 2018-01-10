a0c702224acff86c197ef9e0e42093d60ed38f74
NataliaVerchenko <ver4.nata@gmail.com>
2018-01-10 17:34:59 +0000

    add scroll up

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $('.carusel').slick({
-        autoplay:false,
+        autoplay: false,
         autoplaySpeed: 4500,
-        arrows:false,
-        dots:false,
-        speed:1000
+        arrows: false,
+        dots: false,
+        speed: 1000
 
     });
     $('.list-works').slick({
-        autoplay:false,
-        arrows:true,
-        dots:true,
-        speed:1000,
+        autoplay: false,
+        arrows: true,
+        dots: true,
+        speed: 1000,
         slidesToShow: 4,
         slidesToScroll: 4
 
 
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
     });
 
 });
